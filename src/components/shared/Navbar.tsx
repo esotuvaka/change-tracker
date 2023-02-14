079dfdecf4f43eda3a2339dba8c6da9a0e7917d8
PiqqiDesigns <eric.otuvaka@gmail.com>
2023-02-13 23:43:44 -0800

    fix: Promise<void> fix, styling, Prisma highlights

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -6,24 +6,36 @@ export function Navbar() {
     <>
       <header className="absolute flex h-[10vh] w-full items-center bg-gradient-to-t from-transparent to-black">
         <div className="mx-auto flex w-4/5 justify-between rounded-md bg-neutral-700/70 py-2 px-4 text-xl text-white ring-1 ring-white">
-          <Link href="/" className="hover:cursor-pointer">
+          <Link
+            href="/"
+            className="border-1 border-b border-b-transparent transition-all duration-300 hover:cursor-pointer hover:border-b-white"
+          >
             HOME
           </Link>
           <ul className="flex gap-16">
-            <Link href="/issues" className="hover:cursor-pointer">
+            <Link
+              href="/issues"
+              className="border-1 border-b border-b-transparent transition-all duration-300 hover:cursor-pointer hover:border-b-white"
+            >
               ISSUES
             </Link>
-            <Link href="/changes" className="hover:cursor-pointer">
+            <Link
+              href="/changes"
+              className="border-1 border-b border-b-transparent transition-all duration-300 hover:cursor-pointer hover:border-b-white"
+            >
               CHANGES
             </Link>
-            <Link href="/solved" className="hover:cursor-pointer">
+            <Link
+              href="/solved"
+              className="border-1 border-b border-b-transparent transition-all duration-300 hover:cursor-pointer hover:border-b-white"
+            >
               SOLVED
             </Link>
           </ul>
 
           <Link
             href="/"
-            className="hover:cursor-pointer"
+            className="border-1 border-b border-b-transparent transition-all duration-300 hover:cursor-pointer hover:border-b-white"
             onClick={() => void signOut()}
           >
             LOGOUT
--- src/pages/issues/index.tsx
+++ src/pages/issues/index.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import type { Resolver, SubmitHandler } from "react-hook-form";
+import type { Resolver } from "react-hook-form";
 
 type FormValues = {
   title: string;
@@ -29,25 +29,43 @@ export default function Issues() {
   } = useForm<FormValues>({
     resolver: resolver,
   });
-  const onSubmit = handleSubmit((data) => alert(JSON.stringify(data)));
+  const onSubmit = handleSubmit(
+    (data) => alert(JSON.stringify(data)),
+    (e) => {
+      console.error(e);
+    }
+  );
 
   return (
     <section className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-neutral-700 to-black pt-[10vh]">
       <div className="mx-auto h-[90vh] w-4/5 text-5xl text-white">
-        <div>
-          <h1>Add new issue</h1>
-          <form className="flex flex-col" onSubmit={onSubmit}>
+        <div className="absolute bottom-0 w-4/5">
+          <form
+            className="mx-auto my-8 flex w-3/5 flex-col rounded-md bg-gradient-to-b from-neutral-700 to-black p-8 pt-4 text-2xl"
+            onSubmit={onSubmit}
+          >
+            <h1 className="text-3xl">Add New Issue</h1>
             <label>Title</label>
-            <input {...register("title")} />
+            <input
+              {...register("title")}
+              className="rounded-md bg-neutral-700 p-1 pl-3 ring-1 ring-white"
+            />
             {errors.title ? <p>{errors.title.message}</p> : <></>}
             <label>Description</label>
-            <input {...register("description")} />
-            <label>Environment</label>
             <input
+              {...register("description")}
+              className="rounded-md bg-neutral-700 p-1 pl-3 ring-1 ring-white"
+            />
+            <label>Environment</label>
+            <textarea
               {...register("environment")}
               placeholder="Steps to reproduce the issue"
+              className="rounded-md bg-neutral-700 p-1 pl-3 ring-1 ring-white"
+            ></textarea>
+            <input
+              type="submit"
+              className="mx-auto mt-4 w-32 rounded-md bg-neutral-700 px-4 py-2 ring-1 ring-white transition-all duration-300 hover:cursor-pointer hover:ring-offset-2"
             />
-            <input type="submit" />
           </form>
         </div>
       </div>
